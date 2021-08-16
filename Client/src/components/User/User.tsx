8d17f7af762b3116bb816db96de5c22ffdf67c12
sbrandon0432 <sbrandon0432@yahoo.com>
2021-08-15 21:46:31 -0700

    code cleanup and css

--- Client/src/components/User/User.tsx
+++ Client/src/components/User/User.tsx
@@ -2,111 +2,64 @@ import React, {useEffect, useState} from "react";
 import { Button } from "react-bootstrap";
 import './UserS.scss';
 import MoreGrades from "../MoreGrades/MoreGrades";
-const User = (props: any ) => {
+import Name from "../Name/Name";
 
+interface IStudent {
+  firstName: string;
+  email: string;
+  company: string;
+  pic: string;
+  skill: string;
+  grades: number[];
+}
 
-  //console.log(props.student)
-  const [studentName, chStudentName] = useState(props.student.firstName);
-  const [editName, chName] = useState(false);
+interface Props {
+  student: IStudent;
+}
+
+const studentAverage = (grades: number[]) => {
+
+  let sum = 0;
+  for (let grade in grades) {
+    let number = Number(grades[grade]);
+    sum += number;
+  }
+  return sum/grades.length;
+
+}
+
+const User = ({ student }: Props ) => {
   const [moreInfo, chMoreInfo] = useState(false);
 
-  const studentPicture = props.student.pic;
-  const studentEmail = props.student.email;
-  const studentCompany = props.student.company;
-  const studentSkill = props.student.skill
-  const studentGrades = props.student.grades;
+  const { firstName, email, company, pic, skill, grades } = student
 
-  const studentAverage = () => {
 
-    let sum = 0;
-    for (let grade in studentGrades) {
-      let number = Number(studentGrades[grade]);
-      sum += number;
-    }
-    return sum/studentGrades.length;
-
-  }
-
-  if (editName) {
-    return (
-
-      <div className = "User">
-      <img className="Student-Picture" src = {studentPicture} />
+  return (
+    <div className = "User">
+      <img className="Student-Picture" src = {pic} />
       <div className="User-Info">
 
-      <div className= "Student-Name" > { studentName } </div>
+        <Name name={firstName}/>
 
-      <form className="Name-Change" onSubmit={(e)=>{
-        e.preventDefault();
-        chName(false);
-      }}>
-        <label >
-          New Name:
-        </label>
-        <input  type='text' onChange={ (e) => {
-          chStudentName(e.target.value)
-        }}/>
-        <button type='submit' >Save Changes</button>
-      </form>
+        <div className="More-Info">
+          <div className="Student-Email" > Email: { email } </div>
+          <div className="Student-Skill" > Skill: { skill } </div>
+          <div className="Student-Average"> Average: {studentAverage(grades)}%  </div>
 
-        <div className="Student-Email" > Email: { studentEmail } </div>
-        <div className="Student-Skill" > Skill: { studentSkill } </div>
-        <div className="Student-Average"> Average: {studentAverage()}%  </div>
-      </div>
-    </div>
-
-
-    );
-  }
-
-  else if (moreInfo) {
-    return (
-
-      <div className = "User">
-      <img className="Student-Picture" src = {studentPicture} />
-      <div className="User-Info">
-        <div className= "Student-Name" > { studentName } </div>
-        <div className="Student-Email" > Email: { studentEmail } </div>
-        <div className="Student-Skill" > Skill: { studentSkill } </div>
-        <div className="Student-Average"> Average: {studentAverage()}%  </div>
-      </div>
-        <div className="More-Grades">
-          <button onClick={(e)=>{chMoreInfo(false)}} >-</button>
-        </div>
-
-        <div className="grades">
-         < MoreGrades grades={studentGrades}/>
-        </div>
-
-    </div>
-    )
-
-  }  else {
-
-    return (
-      <div className = "User">
-        <img className="Student-Picture" src = {studentPicture} />
-        <div className="User-Info">
-
-        <div className= "Student-Name" >
-        { studentName }
-         <button onClick={() => {
-           chName(true);
-         }}>:Edit</button>
-
-        </div>
-          <div className="Student-Email" > Email: { studentEmail } </div>
-          <div className="Student-Skill" > Skill: { studentSkill } </div>
-          <div className="Student-Average"> Average: {studentAverage()}%  </div>
-        </div>
-          <div className="More-Grades">
-            <button onClick={(e)=>{chMoreInfo(true)} }>+</button>
+          <div className="More-Grades-Button">
+            <button onClick={(e)=>{chMoreInfo(!moreInfo)} }>{moreInfo ? "-" : "+"}</button>
           </div>
+          {moreInfo && (
+              <div className="grades">
+                <MoreGrades grades={grades}/>
+              </div>
+          )}
+        </div>
       </div>
-    );
-  }
 
 
+    </div>
+  );
 }
 
 export default User;
