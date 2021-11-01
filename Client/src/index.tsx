522ba9e7d6e2f44ad18ab229ed0af8dcaeb1726b
sbrandon0432 <sbrandon0432@yahoo.com>
2021-11-01 15:41:36 -0700

    p

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,11 +1,18 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './indexS.scss';
+import UserCards from './components/UserCards/UserCards';
+import Sandbox from './/Sandbox/Sandbox'
+
+
+
+
 
 ReactDOM.render(
+
   <StrictMode>
     <div className="App">
-        IM working!
+    <Sandbox />
     </div>
   </StrictMode>,
 
