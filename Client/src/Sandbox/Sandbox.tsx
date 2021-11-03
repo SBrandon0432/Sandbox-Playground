02da88c1d1d5c514c2c8e5f2a571d5ca04b389b6
sbrandon0432 <sbrandon0432@yahoo.com>
2021-11-02 17:35:31 -0700

    api working for movies and key add for api

--- Client/src/Sandbox/Sandbox.tsx
+++ Client/src/Sandbox/Sandbox.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './SandBoxS.scss';
+import api from './key';
 
 export default function SandBox () {
   const [movies, setMovies] = useState([]);
 
   useEffect( () => {
     async function getRequest() {
-      const response = await fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr');
+      const response = await fetch(`https://imdb-api.com/en/API/MostPopularMovies/${api}`);
       const json = await response.json();
       console.log(json, 'json')
-      setMovies(json);
+      setMovies(json.items);
 
     }
     getRequest();
 
   }, [])
-  // console.log(movies)
+   console.log(movies)
   return (
     <div className = 'SandBox'>
       test
